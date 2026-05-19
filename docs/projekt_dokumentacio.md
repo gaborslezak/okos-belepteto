f2f1a81d1f7fbe9236e7de84406ea2b6cb18c373
Gabor Slezak <gaborslezak@protonmail.com>
2026-05-19 19:08:25 +0200

    Add keypad code and update project docs

--- docs/projekt_dokumentacio.md
+++ docs/projekt_dokumentacio.md
@@ -24,9 +24,23 @@ Az okos beléptető célja egy olyan proof of concept rendszer kialakítása, am
 #### LCD bekötése:
 
 ![LCD-Arduino bekötés](imgs/LCD-Arduino_bekotes.png)
-|LCD|Arduino Uno R3|
+|LCD Pin|Arduino Uno R3 Pin|
 |-----|--------------|
 |SCL|A5|
 |SDA|A4|
 |VCC|5V|
 |GND|GND|
+
+#### Keypad bekötése:
+
+![LCD-Arduino bekötés](imgs/Keypad-Arduino_bekotes.png)
+|Keypad Pin|Arduino Uno R3 Pin|
+|-----|--------------|
+|Row 1|11|
+|Row 2|10|
+|Row 3|9|
+|Row 4|8|
+|Column 1|7|
+|Column 2|6|
+|Column 3|5|
+|Column 4|4|
--- src/arduino_uno_r3.cpp
+++ src/arduino_uno_r3.cpp
@@ -1,6 +1,22 @@
 #include <LiquidCrystal_I2C.h>
+#include <Keypad.h>
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+const byte ROWS = 4;
+const byte COLUMNS = 4;
+
+char keys[ROWS][COLUMNS] = {
+  {'1', '2', '3', 'A'},
+  {'4', '5', '6', 'B'},
+  {'7', '8', '9', 'C'},
+  {'*', '0', '#', 'D'}
+};
+
+byte rowPin[ROWS] = {11, 10, 9, 8};
+byte colPin[COLUMNS] = {7, 6, 5, 4};
+
+Keypad keypad = Keypad(makeKeymap(keys), rowPin, colPin, ROWS, COLUMNS);
 
 void udvozles() {
   lcd.setCursor(0, 0);
@@ -13,10 +29,12 @@ void udvozles() {
 void setup() {
   lcd.init();
   lcd.backlight();
-  udvozles();
+
 }
 
 void loop() {
-  lcd.scrollDisplayLeft();
-  delay(450);
+  char key = keypad.getKey();
+  if (key) {
+    udvozles();
+  }
 }
